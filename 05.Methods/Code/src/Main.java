96318e677e83103c2fb43e74168fce36a6c57f5d
P Shiva Shankar <shivashankarahsrav@gmail.com>
2024-01-24 15:36:12 +0530

    Methods

---
+++ 05.Methods/Code/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //Creating The Greet Function
+    public static void Greet()
+    {
+        System.out.println("Methods In Java");
+    }
+    public static void main(String[] args) {
+    //calling the Geet function from main
+        Greet();
+    }
+}
